b498cc66034e72711e37ca33a02dfc9a2f862997
Nassim Chakroun <75129418+nassimch12@users.noreply.github.com>
2024-12-11 18:50:50 +0100

    Time: 0 ms (100.00%) | Memory: 12.2 MB (10.07%) - LeetSync

---
+++ 70-climbing-stairs/climbing-stairs.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def climbStairs(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        
+        if n == 0:
+            return 1
+        elif n == 1:
+            return 1
+
+        a, b = 1, 1
+
+        for i in range(2, n + 1):
+            a, b = b, a + b
+
+        return b
